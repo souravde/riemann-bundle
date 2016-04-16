410edc752408a569898a07c5930f13083a387906
phaneesh <phaneesh.n@gmail.com>
2016-04-16 08:42:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# Dropwizard XML Bundle [![Travis build status](https://travis-ci.org/phaneesh/riemann-bundle.svg?branch=master)](https://travis-ci.org/phaneesh/riemann-bundle)
+
+This bundle simplifies integrating dropwizard metrics with [Riemann](http://riemann.io/).
+This bundle compiles only on Java 8.
+ 
+## Dependencies
+* [metrics3-riemann-reporter](https://github.com/riemann/riemann-java-client/tree/master/metrics3-riemann-reporter) 0.4.1 
+
+## Usage
+The bundle integrates dropwizard metrics with [Riemann](http://riemann.io/) with a simple configuration. 
+ 
+### Build instructions
+  - Clone the source:
+
+        git clone github.com/phaneesh/riemann-bundle
+
+  - Build
+
+        mvn install
+
+### Maven Dependency
+Use the following repository:
+```xml
+<repository>
+    <id>clojars</id>
+    <name>Clojars repository</name>
+    <url>https://clojars.org/repo</url>
+</repository>
+```
+Use the following maven dependency:
+```xml
+<dependency>
+    <groupId>io.dropwizard.riemann</groupId>
+    <artifactId>riemann-bundle</artifactId>
+    <version>0.9.2-SNAPSHOT</version>
+</dependency>
+```
+
+### Using Riemann bundle
+
+#### Bootstrap
+```java
+    @Override
+    public void run(Configuration configuration, Environment environment) {
+        bootstrap.addBundle(new RiemannBundle() {
+            
+            @Override
+            public RiemannConfig getRiemannConfiguration(Configuration configuration) {
+                ...
+            }
+        });
+    }
+```
+
+LICENSE
+-------
+
+Copyright 2016 Phaneesh Nagaraja <phaneesh.n@gmail.com>.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ src/main/java/io/dropwizard/riemann/RiemannBundle.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2016 Phaneesh Nagaraja <phaneesh.n@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.dropwizard.riemann;
+
+import com.codahale.metrics.riemann.Riemann;
+import com.codahale.metrics.riemann.RiemannReporter;
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
+import io.dropwizard.Configuration;
+import io.dropwizard.ConfiguredBundle;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author phaneesh
+ */
+@Slf4j
+public abstract class RiemannBundle<T extends Configuration> implements ConfiguredBundle<T> {
+
+    private static Riemann riemann;
+
+    public abstract RiemannConfig getRiemannConfiguration(T configuration);
+
+    @Override
+    public void initialize(Bootstrap<?> bootstrap) {
+
+    }
+
+    @Override
+    public void run(T configuration, Environment environment) {
+        if (riemann == null) {
+            final val riemannConfig = getRiemannConfiguration(configuration);
+            try {
+                riemann = new Riemann(riemannConfig.getHost(), riemannConfig.getPort());
+                final val tags = ImmutableList.<String>builder()
+                        .add(riemannConfig.getNamespace())
+                        .add(riemannConfig.getEnvironment())
+                        .add(riemannConfig.getService())
+                        .addAll(riemannConfig.getTags())
+                        .build();
+                RiemannReporter.Builder builder = RiemannReporter.forRegistry(environment.metrics()).tags(tags)
+                        .convertDurationsTo(TimeUnit.MILLISECONDS).convertRatesTo(TimeUnit.SECONDS);
+                RiemannReporter riemannReporter = builder.build(riemann);
+                riemannReporter.start(riemannConfig.getPollingInterval(), TimeUnit.SECONDS);
+                log.info("Started Riemann metrics reporter on {}:{} with tags: {}", riemannConfig.getHost(),
+                        riemannConfig.getPort(), Joiner.on(",").join(tags));
+            } catch (IOException e) {
+                log.error("Error starting Riemann reporter", e);
+            }
+        }
+    }
+}
